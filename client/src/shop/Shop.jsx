4b17f9f4baab6a98a024d47d8a81ae81cf13dfe9
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-04-10 01:02:04 +0530

    fixed layout

--- client/src/shop/Shop.jsx
+++ client/src/shop/Shop.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card } from "flowbite-react";
 
 const Shop = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect( () => {
+    fetch("http://localhost:5001/all-books").then(res => res.json()).then(data => setBooks(data));
+  }, [])
   return (
-    <div>
-      Shop
+    <div className='mt-28 px-4 lg:px'>
+      <h2 className='text-5xl font-bold text-center'>All Books are here</h2>
+
+      <div className='grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1'>
+        {
+          books.map(book => <Card
+            className="max-w-xxs flex flex-col"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc={book.imageUrl}
+          >
+            <div className="flex-grow">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {book.bookTitle}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {book.bookDescription}
+            </p>
+            </div>
+            <button className='bg-blue-700 font-semibold text-white py-2 rounded'>Buy Now</button>
+          </Card>)
+        }
+      </div>
+
     </div>
   )
 }
