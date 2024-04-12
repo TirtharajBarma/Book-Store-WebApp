8fd7e4244a5c8ddbd4d33d352c29338b04451ba2
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-04-12 18:52:36 +0530

    changed the book slice

--- client/src/home/OtherBooks.jsx
+++ client/src/home/OtherBooks.jsx
@@ -5,7 +5,7 @@ const OtherBooks = () => {
   const [books, setBooks] = useState([]);
 
   useEffect( () => {
-      fetch("http://localhost:5001/all-books").then(res => res.json()).then(data => setBooks(data.slice(1, 5)))
+      fetch("http://localhost:5001/all-books").then(res => res.json()).then(data => setBooks(data.slice(5, 10)))
   }, [])
 
   return (
