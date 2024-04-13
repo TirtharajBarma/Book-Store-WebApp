464c070e28ae87bf0f9f9f164a2a3a174b8aa016
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-04-13 18:26:37 +0530

    enable private routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* text from react-router dom website */}
-    <RouterProvider router={router} />    
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
