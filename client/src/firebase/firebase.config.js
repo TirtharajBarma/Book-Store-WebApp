0b52aa2e45088013b49dae14a3d9b1415652f1b5
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-04-13 18:28:48 +0530

    firebase integration

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJvEjm5NUwEgIX7mc_vGylUNBdmJ06C58",
+  authDomain: "book-store-40088.firebaseapp.com",
+  projectId: "book-store-40088",
+  storageBucket: "book-store-40088.appspot.com",
+  messagingSenderId: "618200856766",
+  appId: "1:618200856766:web:0a3921157f561a798806ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
