99c420e469359bc723d2f6d494205dfff890f229
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-10-08 23:23:26 +0530

    add vercel configuration and update server port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
